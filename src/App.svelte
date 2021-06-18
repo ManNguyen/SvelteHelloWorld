8a1b6e7c0e6fbd09ef6cd1454b62dec5bb58b641
ManNguyen <7028401+ManNguyen@users.noreply.github.com>
2021-06-18 16:00:54 +0700

    add tailwind css

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,28 @@
 <script>
-	export let name;
+  import Header from "./layout/Header.svelte";
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <Header name="man" />
+  <p>
+    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+    how to build Svelte apps.
+  </p>
+
+
+  <div class="grid grid-flow-col grid-cols-3 grid-rows-3 gap-4">
+	<div>1</div>
+	<div>2</div>
+	<div>3</div>
+	<div>4</div>
+	<div>5</div>
+	<div>6</div>
+	<div>7</div>
+	<div>8</div>
+	<div>9</div>
+  </div>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+ 
 </style>
---
+++ src/layout/Header.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let name;
+</script>
+
+<h1>This is {name}</h1>
+
+<style>
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+</style>
